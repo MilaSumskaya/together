79aa8cd6f006b421d5554d64b9fc856187c9f247
Mila <mila.sumskaya04@mail.ru>
2023-09-07 15:12:47 +0300

    20

--- application/views/application.php
+++ application/views/application.php
@@ -3,22 +3,24 @@
     <div class="col-1">
     </div>
     <div class="col-10">
-        <div class="header-h1 header-h1-center">
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..."  alt="...">
-    </div>
-    <div class="col-md-8">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <?php
+  foreach($app as $row) {
+      echo '
+  <div class="col">
+    <div class="card">
+    <img src="img/'.$row['before_photo'].'" class="card-img-top"  alt="Выполненная заявка '.$row['id'].'">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <h5 class="card-title">'.$row['name'].'</h5>
+      <p class="card-text">'.$row['description'].'</p>
+      <p class="card-text"><small class="text-muted"> От '.$row['submission_date'].' до '.$row['completion_date'].'</small></p>
       </div>
     </div>
   </div>
+      ';} ?>
 </div>
-      </div>
+    </div>
+    <div class="col-1">
     </div>
   </div>
 </div>
