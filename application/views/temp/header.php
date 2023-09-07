3ebfec134628b890f90fe5a6fe3832f2257fedc4
Mila <mila.sumskaya04@mail.ru>
2023-09-07 11:18:39 +0300

    15

--- application/views/temp/header.php
+++ application/views/temp/header.php
@@ -3,9 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Сделаем лучше вместе</title>
 </head>
 <body>
-    
-</body>
-</html>
+  <header>
+    <div class="sample-header">
+  <div class="sample-header-section">
+    <h1>Городской портал "Сделаем лучше вместе"</h1>
+    <h2>Прием заявок на устранение проблем в городе</h2>
+  </div>
+</div>
+  </header>
---
+++ application/views/temp/navbar.php
@@ -0,0 +1,15 @@
+<div class="sample-section-wrap">
+<nav>
+<ul class="menu-main">
+  <li><a href="" class="current">Главная</a></li>
+  <li><a href="reg.php">Авторизация</a></li>
+  <li><a href="">Регистрация</a></li>
+</ul>
+</nav>
+  <div class="sample-section">
+    <p>Количество решенных заявок: </p>
+
+</div>
+</div>
+</body>
+</html>
